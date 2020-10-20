801be68f6b1631c8db12b31ab0e426a0625e745a
Mauro <mpasserino@irobot.com>
2020-10-20 15:43:15 +0100

    Rename ExecutorEventCallback -> EventsExecutorCallback

--- rmw/include/rmw/rmw.h
+++ rmw/include/rmw/rmw.h
@@ -2794,7 +2794,7 @@ RMW_WARN_UNUSED
 rmw_ret_t
 rmw_subscription_set_events_executor_callback(
   const void * executor_context,
-  ExecutorEventCallback executor_callback,
+  EventsExecutorCallback executor_callback,
   const void * subscription_handle,
   void * rmw_subscription);
 
@@ -2804,7 +2804,7 @@ RMW_WARN_UNUSED
 rmw_ret_t
 rmw_service_set_events_executor_callback(
   const void * executor_context,
-  ExecutorEventCallback executor_callback,
+  EventsExecutorCallback executor_callback,
   const void * service_handle,
   void * rmw_service);
 
@@ -2814,7 +2814,7 @@ RMW_WARN_UNUSED
 rmw_ret_t
 rmw_client_set_events_executor_callback(
   const void * executor_context,
-  ExecutorEventCallback executor_callback,
+  EventsExecutorCallback executor_callback,
   const void * client_handle,
   void * rmw_client);
 
@@ -2824,7 +2824,7 @@ RMW_WARN_UNUSED
 rmw_ret_t
 rmw_guard_condition_set_events_executor_callback(
   const void * executor_context,
-  ExecutorEventCallback executor_callback,
+  EventsExecutorCallback executor_callback,
   const void * guard_condition_handle,
   void * rmw_guard_condition,
   bool use_previous_events);
@@ -2835,7 +2835,7 @@ RMW_WARN_UNUSED
 rmw_ret_t
 rmw_event_set_events_executor_callback(
   const void * executor_context,
-  ExecutorEventCallback executor_callback,
+  EventsExecutorCallback executor_callback,
   const void * event_handle,
   void * rmw_event,
   bool use_previous_events);
